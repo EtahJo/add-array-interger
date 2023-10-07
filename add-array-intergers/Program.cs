6af044a7f14a336598de6d997ef7a08ccf8a9d58
Etah <arrahetah23@gmail.com>
2023-10-07 13:55:40 +0100

    first commit

---
+++ add-array-intergers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace AddArrayIntergers
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int[] intergersArray = new int[4];
+
+            int result = 0;
+            int presentIndex = 0;
+            while (presentIndex <= intergersArray.Length-1)
+            {
+                Console.Write(": ");
+                intergersArray[presentIndex] = int.Parse(Console.ReadLine());
+                presentIndex += 1;
+            }
+             presentIndex = 0;
+
+            while (presentIndex <= intergersArray.Length-1)
+            {
+                result += intergersArray[presentIndex];
+                presentIndex += 1;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
